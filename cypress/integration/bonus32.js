828bd7d16e131d6202dff924389c198297d7c17f
jjhelguero <jjhelguero@protonmail.com>
2022-08-26 11:02:57 -0500

    add more test

--- cypress/integration/bonus32.js
+++ cypress/integration/bonus32.js
@@ -3,6 +3,7 @@
 it('waits for each response one by one', () => {
   // visit the page "burst.html"
   // https://on.cypress.io/visit
+  cy.visit('/burst.html')
   //
   // keep track of the the total number of calls
   // by "spying" on the network call GET /random-digit
@@ -10,9 +11,13 @@ it('waits for each response one by one', () => {
   // https://on.cypress.io/intercept
   // and give it an alias "api"
   let calls = 0
+  cy.intercept('GET','/random-digit', () => {
+    calls += 1
+  }).as('api')
   //
   // click on the button with ID "burst"
   // https://on.cypress.io/click
+  cy.get('#burst').click()
   //
   // we know the calls will be made within 3 seconds
   // thus we "simply" wait for 3 seconds using
@@ -21,6 +26,15 @@ it('waits for each response one by one', () => {
   // to call the "cy.wait(api alias)" the number of times
   // our spy intercepted was called
   // https://on.cypress.io/then
+  cy.wait(3_000).then(() => {
+    for(let k = 0; k < calls; k++ ) {
+      cy.wait('@api')
+    }
+  })
+    .its('response.body.n')
+    .then((n) => {
+      cy.contains('#api-response', `${calls} said ${n}`)
+    })
   //
   // get the subject yielded by the last cy.wait(api alias)
   // and grab its response "n" value
@@ -35,6 +49,7 @@ it('waits for each response one by one', () => {
 it('waits for each response one by one using a spy', () => {
   // visit the page "burst.html"
   // https://on.cypress.io/visit
+  cy.visit('/burst.html')
   //
   // keep track of the the total number of calls
   // by "spying" on the network call GET /random-digit
@@ -45,9 +60,11 @@ it('waits for each response one by one using a spy', () => {
   // give the cy.stub an alias "calls"
   // Note: even if we passed a cy.stub instance to cy.intercept
   // it does not STUB the network call, it still goes to the server
+  cy.intercept('GET', '/random-digit', cy.stub().as('calls')).as('api')
   //
   // click on the button with ID "burst"
   // https://on.cypress.io/click
+  cy.get('#burst').should('be.visible').click()
   //
   // we know the calls will be made within 3 seconds
   // thus we "simply" wait for 3 seconds using
@@ -56,6 +73,27 @@ it('waits for each response one by one using a spy', () => {
   // to call the "cy.wait(api alias)" the number of times
   // our spy intercepted was called
   // https://on.cypress.io/then
+  cy.wait(3_000)
+  cy.get('@calls')
+    .its('callCount')
+    .then(calls => {
+      for(let k = 0; k < calls; k++ ) {
+        cy.wait('@api')
+      }
+    })
+    .its('response.body.n')
+    .then((n) => {
+      cy.get('@calls')
+        .its('callCount')
+        .then((callCount) => {
+          // confirm the page contains the expected text output
+          // something like "last call index said N"
+          cy.contains(
+            '#api-response',
+            `${callCount} said ${n}`,
+          )
+    })
+  })
   //
   // get the cy.stub using its alias
   // and its property "callCount"
@@ -80,18 +118,26 @@ it('waits for each response one by one using a spy', () => {
 it('inspects the last intercepted API call', () => {
   // visit the page "burst.html"
   // https://on.cypress.io/visit
+  cy.visit('/burst.html')
   //
   // spy on the "GET /random-digit" calls
   // and give it an alias "api"
   // https://on.cypress.io/intercept
+  cy.intercept('GET', '/random-digit').as('api')
   //
   // click on the button with ID "burst"
   // https://on.cypress.io/click
+  cy.get('#burst').should('be.visible').click().wait(3_000)
   //
   // we know the calls will be made within 3 seconds
   // thus we "simply" wait for 3 seconds using
   // https://on.cypress.io/wait
   // Tip: we could use https://github.com/bahmutov/cypress-network-idle
+  cy.get('@api.all')
+    .then((list) => {
+      const n = Cypress._.last(list).response.body.n
+      cy.contains('#api-response', `${list.length} said ${n}`)
+    })
   //
   // get all intercepts at once using the "@api.all" syntax
   // and from that list (which should be final by now)
