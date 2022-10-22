1fc3d2b31683390336ad6b1f8707e930d000fc7a
jjhelguero <joshua.helguero@mercari.com>
2022-10-22 11:30:43 -0500

    add test 38

--- cypress/integration/bonus38.js
+++ cypress/integration/bonus38.js
@@ -18,8 +18,18 @@ it('checks the intercepts until it gets the right response', () => {
   // check if any of them have response object with "n: 7" property
   // wait up to 30 seconds, check every second
   // tip: use cypress-recurse
+  recurse(
+    () => cy.get('@random.all'),
+    (intercepts) => intercepts.some(x => x.response.body.n === 7),
+    {
+      timeout: 30_000,
+      delay:1_000,
+      log: 'found n=7'
+    }
+  )
   //
   // once we have confirmed the network call with "n: 7",
   // the page is showing 7 immediately
   // which we can verify using cy.contains with a very short timeout
+  cy.contains('#number', '7', {timeout: 100}).should('be.visible')
 })
