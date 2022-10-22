3cfd098070705e27a46986726292310a8017f82e
jjhelguero <joshua.helguero@mercari.com>
2022-10-22 12:11:08 -0500

    add test 40

--- cypress/integration/bonus40.js
+++ cypress/integration/bonus40.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 // custom command definitions from the "cypress-network-idle" plugin
+/// <reference types="cypress-network-idle" />
 // load the custom commands from the "cypress-network-idle" plugin
+import 'cypress-network-idle'
 
 it('waits for network calls to finish', () => {
   // We want to visit the page "/calculator.html"
@@ -11,10 +13,20 @@ it('waits for network calls to finish', () => {
   // https://github.com/bahmutov/cypress-network-idle
   //
   // now visit the /calculator.html page
+  cy.waitForNetworkIdlePrepare({
+    method: 'POST',
+    pattern: '*',
+    alias: 'track'
+  })
   cy.visit('/calculator.html')
   //
   // wait for all possible tracking calls
   // plus 1 second of idle time
+  cy.waitForNetworkIdle('@track', 1_000)
+    .then(({callCount, waited}) => {
+      expect(callCount, 'call count').to.be.within(1,3)
+      expect(waited, 'waited').to.be.above(1000)
+    })
   //
   // log the yielded information about the wait
   // using console.log + cy.then
