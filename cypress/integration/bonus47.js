a4549dbac0dd22ad784666b44a700ad06afed7f6
jjhelguero <joshua.helguero@mercari.com>
2023-05-31 22:24:08 -0500

    add new tests

--- cypress/integration/bonus47.js
+++ cypress/integration/bonus47.js
@@ -4,6 +4,11 @@ it('shows the returned JSONP response', () => {
   // intercept the GET requests to "/api-jsonp" endpoint
   // that looks like /api-jsonp?callback=...
   // https://on.cypress.io/intercept
+  cy.intercept('/api-jsonp*',(req) => {
+    req.continue((res) => {
+      console.log(res.body)
+    })
+  }).as('jsonp')
   //
   // let the request continue going to the server
   // and print the response body to show it is a string
@@ -13,8 +18,11 @@ it('shows the returned JSONP response', () => {
   //
   // visit the "/jsonp.html" page
   // and confirm the call "jsonp" has happened
+  cy.visit('/jsonp.html')
+  cy.wait('@jsonp')
   //
   // confirm there are two items shown in the list of names
+  cy.get('li').should('have.length',2)
 })
 
 it('stubs a JSONP call', () => {
@@ -25,11 +33,35 @@ it('stubs a JSONP call', () => {
   ]
   // intercept the ajax calls to the "/api-json" endpoint
   //
+   cy.intercept({
+    method: 'GET', 
+    pathname: '/api-jsonp'
+   }, (req) => {
+      const callbackName = req.query.callback
+      if (!callbackName) {
+        console.error(req.query)
+        throw new Error('Missing the JSONP callback name')
+      }
+      req.reply({
+        headers: {
+          'content-type': 'application/x-javascript; charset=utf-8',
+        },
+        body:
+          callbackName +
+          '(' +
+          JSON.stringify(testData) +
+          ')',
+      })
+   }).as('jsonp')
   // see the entire object of parameters
   // console.log(req.query)
   // look at the request object
   // grab the callback name from the request query object
   // if there is no callback name, throw an error
+  cy.visit('/jsonp.html')
+  cy.wait('@jsonp')
+
+  cy.get('li').should('have.length', 1).first().should('have.text', 'Cy')
   //
   // reply with a JavaScript snippet
   // that calls the function name set by the user
